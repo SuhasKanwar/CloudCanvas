caf2abbc63b86e44a915dbecdd1ed6bd9d5441d0
SuhasKanwar <suhas.kanwar@gmail.com>
2026-08-16 00:31:30 +0530

    Added the auth setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,23 @@
 import express, { type Request, type Response } from 'express';
 import type { ApiResponse } from './types/response.js';
 import { PORT } from './lib/config.js';
+import cors from 'cors';
+import logger from './middlewares/logger.js';
+import authRouter from './routes/authRouter.js';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+
+app.use(cors({
+    origin: '*'
+}));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(logger);
 
 app.get("/", (_req: Request, res: Response<ApiResponse>) => {
     res.json({
@@ -17,6 +32,8 @@ app.get("/health", (_req: Request, res: Response<ApiResponse>) => {
         message: "CloudCanvas server is healthy."
     });
 });
+
+app.use("/api/auth", authRouter);
 
 app.listen(PORT, (err) => {
     if(err) {
