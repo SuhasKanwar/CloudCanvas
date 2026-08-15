b459f1c946771abbd38adc498f5ba28c78bbe91c
SuhasKanwar <suhas.kanwar@gmail.com>
2026-08-15 23:17:27 +0530

    added db setup

--- server/src/lib/config.ts
+++ server/src/lib/config.ts
@@ -1,1 +1,2 @@
 export const PORT: number = Number(process.env.PORT) || 9000;
+export const DATABASE_URL: string = process.env.DATABASE_URL || "postgresql://postgres:dev@localhost:5432/cloudcanvas";
